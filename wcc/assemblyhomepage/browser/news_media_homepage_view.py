20534bad913a3bdbe05cb12679806729b28762fc
Izhar Firdaus <izhar@inigo-tech.com>
2013-10-08 19:51:30 +0800

    item displayed as first item on news-media page should be featured news

--- wcc/assemblyhomepage/browser/news_media_homepage_view.py
+++ wcc/assemblyhomepage/browser/news_media_homepage_view.py
@@ -1,7 +1,6 @@
 from five import grok
 from plone.directives import dexterity, form
 from wcc.assemblyhomepage.content.news_media_homepage import INewsMediaHomepage
-
 grok.templatedir('templates')
 
 class Index(dexterity.DisplayForm):
@@ -10,12 +9,27 @@ class Index(dexterity.DisplayForm):
     grok.template('news_media_homepage_view')
     grok.name('view')
 
+    def featured_item(self):
+        rel = self.context.news_source
+        if not rel:
+            return []
+        source = rel.to_object
+        results = source.queryCatalog(batch=False) or []
+        for i in results:
+            obj = i.getObject()
+            if obj.is_featured:
+                return obj
+        return None            
+
     def news_items(self):
         rel = self.context.news_source
         if not rel:
             return []
         source = rel.to_object
         results = source.queryCatalog(batch=False) or []
+        featured = self.featured_item()
+        if featured:
+            results = [i for i in results if i.UID != featured.UID()]
         return [i.getObject() for i in results[:5]]
 
     def more_news_target(self):
