8cee290fa79b248d1c1ce3a72d7a5492a95012ff
Izhar Firdaus <izhar@inigo-tech.com>
2013-10-24 17:04:50 +0800

    remove to3 profile upgrade

--- wcc/assemblyhomepage/upgrades/handlers.py
+++ wcc/assemblyhomepage/upgrades/handlers.py
@@ -3,25 +3,6 @@ from Products.CMFCore.utils import getToolByName
 
 # -*- extra stuff goes here -*- 
 
-
-@gs.upgradestep(title=u'Upgrade wcc.assemblyhomepage to 3',
-                description=u'Upgrade wcc.assemblyhomepage to 3',
-                source='2', destination='3',
-                sortkey=1, profile='wcc.assemblyhomepage:default')
-def to3(context):
-    setup = getToolByName(context, 'portal_setup')
-    setup.runAllImportStepsFromProfile('profile-wcc.assemblyhomepage.upgrades:to3')
-    catalog = getToolByName(context, 'portal_catalog')
-
-    for brain in catalog(portal_type=[
-            'wcc.assemblyhomepage.homepage',
-            'wcc.assemblyhomepage.newsmediahomepage'],
-            Language='all'):
-
-        obj = brain.getObject()
-        obj.reindexObject()
-
-
 @gs.upgradestep(title=u'Upgrade wcc.assemblyhomepage to 2',
                 description=u'Upgrade wcc.assemblyhomepage to 2',
                 source='1', destination='2',
--- wcc/assemblyhomepage/upgrades/profiles.py
+++ wcc/assemblyhomepage/upgrades/profiles.py
@@ -2,11 +2,6 @@ from collective.grok import gs
 
 # -*- extra stuff goes here -*- 
 
-gs.profile(name=u'to3',
-           title=u'Upgrade wcc.assemblyhomepage to 3',
-           description='',
-           directory='profiles/to3')
-
 gs.profile(name=u'to2',
            title=u'Upgrade wcc.assemblyhomepage to 2',
            description='',
